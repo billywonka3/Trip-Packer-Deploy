48e174fdc99bd18779d780a53d8fcee028f3e096
billywonka3 <billywonka3@gmail.com>
2019-10-13 21:55:08 -0400

    update

--- server/public/precache-manifest.3e8ab77638c95c144930b590028cfa93.js
+++ server/public/precache-manifest.3e8ab77638c95c144930b590028cfa93.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "688de26b57bbc008d0ee4c997a8cdbb9",
+    "revision": "b720322115816b88ce46950d6597d06b",
     "url": "/index.html"
   },
   {
-    "revision": "8a0fc8d243d4459a8ba9",
-    "url": "/static/css/main.d8a63a06.chunk.css"
+    "revision": "a235c0e8a0ab39fd3021",
+    "url": "/static/css/main.5457f0f9.chunk.css"
   },
   {
     "revision": "f3d8a4510209e6429490",
     "url": "/static/js/2.7d1e0613.chunk.js"
   },
   {
-    "revision": "8a0fc8d243d4459a8ba9",
+    "revision": "a235c0e8a0ab39fd3021",
     "url": "/static/js/main.bde67852.chunk.js"
   },
   {
